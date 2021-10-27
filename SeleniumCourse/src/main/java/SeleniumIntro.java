cf058f7e412e30fc259cf0b74f2617229409e75a
Jessica Luz <jessicaluz@gec.inatel.br>
2021-10-26 23:05:28 -0300

    Initializing implementation for Selenium Course

---
+++ SeleniumCourse/src/main/java/SeleniumIntro.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntro {
+
+	public static void main(String[] args) {
+	
+		/*Using interface Webdriver so we dont need to implement one
+		objetc to each broswer*/
+		
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
