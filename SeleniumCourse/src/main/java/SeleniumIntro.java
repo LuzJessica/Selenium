e0b3c67b4af8c84da876344adc344f59e98f108b
Jessica Luz <jessicaluz@gec.inatel.br>
2021-10-29 13:53:45 -0300

    Now adding implementation to open Firefox broswer

--- SeleniumCourse/src/main/java/SeleniumIntro.java
+++ SeleniumCourse/src/main/java/SeleniumIntro.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIntro {
 
@@ -9,8 +10,17 @@ public class SeleniumIntro {
 		objetc to each broswer*/
 		
 		//Setting chrome driver path, key = key of interface selenium webdriver and value = the path in local system where the driver is
+		
+		/* Implementation for Chrome Broswer
+		
 		System.setProperty("webdriver.com.driver", "C:\\Users\\Alex\\SeleniumCourse\\SeleniumCourse\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		*/
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Alex\\SeleniumCourse\\SeleniumCourse\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
 		driver.get("https://rahulshettyacademy.com/");
 		System.out.println(driver.getTitle());//get the title of the website mentioned in line command on line 14 e shows it
 		System.out.println(driver.getCurrentUrl());//get the current url when the browser comes up
