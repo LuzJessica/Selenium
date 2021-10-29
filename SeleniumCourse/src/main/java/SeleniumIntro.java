e5312cc5b77305e31e16ebc8ebfb044504dbd9d4
Jessica Luz <jessicaluz@gec.inatel.br>
2021-10-29 11:47:25 -0300

    Adding driver and doing some initial executions with selenium

--- SeleniumCourse/src/main/java/SeleniumIntro.java
+++ SeleniumCourse/src/main/java/SeleniumIntro.java
@@ -8,8 +8,13 @@ public class SeleniumIntro {
 		/*Using interface Webdriver so we dont need to implement one
 		objetc to each broswer*/
 		
+		//Setting chrome driver path, key = key of interface selenium webdriver and value = the path in local system where the driver is
+		System.setProperty("webdriver.com.driver", "C:\\Users\\Alex\\SeleniumCourse\\SeleniumCourse\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());//get the title of the website mentioned in line command on line 14 e shows it
+		System.out.println(driver.getCurrentUrl());//get the current url when the browser comes up
+		driver.close();//closes the browser window originaly open, and the driver.quit closes all the windows opened during the execution
 	}
 
 }
