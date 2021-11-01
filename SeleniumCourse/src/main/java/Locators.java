fe8705e9034a4ed1658c317f8bd6232ee5f1c6c1
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-01 15:10:21 -0300

    A little bit about Css Selector and SelectorsHub plugin

--- SeleniumCourse/src/main/java/Locators.java
+++ SeleniumCourse/src/main/java/Locators.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,7 +11,10 @@ public class Locators {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Alex\\\\SeleniumCourse\\\\SeleniumCourse\\\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://sso.teachable.com/secure/9521/identity/login");
+		
+		//setting time to selenium so it can waits for a response and will not show error when could not access an object thar is not ready yet
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		
 		/*
 		 * To find elements we need go to go the web page and click on the element we want to inspect, click with right button
@@ -21,9 +26,12 @@ public class Locators {
 		 * What the example do is, after finding the elements, set the email and password in the fields and, after that
 		 * click on the Login Button to start session.
 		 * */
-		driver.findElement(By.id("email")).sendKeys("jessica.luz@matera.com");
-		driver.findElement(By.id("password")).sendKeys("senhaTeste");
-		driver.findElement(By.name("commit")).click();
+		driver.findElement(By.id("inputUsername")).sendKeys("jessicaluz");
+		driver.findElement(By.name("inputPassword")).sendKeys("senhaTeste");
+		driver.findElement(By.className("signInBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		
+		
 	}
 
 }
