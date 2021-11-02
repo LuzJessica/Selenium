a950db1fd426e1f1eb55f0a149b398e7b9f7a17d
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-02 18:47:58 -0300

    A little bit of Siblings and Parents with XPath

---
+++ SeleniumCourse/src/main/java/Locators3.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators3 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Alex\\\\SeleniumCourse\\\\SeleniumCourse\\\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//Sibling child to parent 
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+
+	}
+
+}
