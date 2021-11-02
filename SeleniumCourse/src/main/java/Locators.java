a1094728973ba56d6fd491ca77c55c157229741f
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-02 18:07:33 -0300

    Passing password dinamically by separeted method and doing logout

--- SeleniumCourse/src/main/java/Locators.java
+++ SeleniumCourse/src/main/java/Locators.java
@@ -10,6 +10,8 @@ public class Locators {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Alex\\\\SeleniumCourse\\\\SeleniumCourse\\\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		String username = "jessicaluz";
+		String wrongPassword = "testPassword";
 
 		//setting time to selenium so it can waits for a response and will not show error when could not access an object thar is not ready yet
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -28,8 +30,8 @@ public class Locators {
 
 		// Login Window
 
-		driver.findElement(By.id("inputUsername")).sendKeys("jessicaluz");
-		driver.findElement(By.name("inputPassword")).sendKeys("senhaTeste");
+		driver.findElement(By.id("inputUsername")).sendKeys(username);
+		driver.findElement(By.name("inputPassword")).sendKeys(wrongPassword);
 		driver.findElement(By.className("signInBtn")).click();
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		driver.findElement(By.linkText("Forgot your password?")).click();
@@ -41,25 +43,36 @@ public class Locators {
 
 		// Forgot Login Window
 
+		String newPassword = Locators.getPassword(driver);		
+		driver.findElement(By.cssSelector("button.go-to-login-btn")).click();
+		Thread.sleep(2000);
+
+
+		// Login using the new password
+
+		driver.findElement(By.xpath("//form/input[@placeHolder='Username']")).sendKeys("jessicaluz");
+		driver.findElement(By.xpath("//form/input[@placeHolder='Password']")).sendKeys(newPassword);
+		driver.findElement(By.xpath("//div/form/div/span/input[@id='chkboxOne']")).click();
+		driver.findElement(By.xpath("//div/form/div/span/input[@id='chkboxTwo']")).click();
+		driver.findElement(By.className("signInBtn")).click();
+		
+		// Logout by clicking in logout button
+		
+		driver.findElement(By.cssSelector("button.logout-btn")).click();
+
+	}
+
+
+	//Getting the password dinamically by another method
+	public static String getPassword(WebDriver driver) {
 		driver.findElement(By.xpath("//input[@placeHolder='Name']")).sendKeys("Jessica");
 		//The line bellow has the [2] argument because the attribute type = Text is not unique
 		driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("jessicaluz@gec.inatel.br");
 		driver.findElement(By.cssSelector("input[placeHolder='Phone Number']")).sendKeys("+5519997351423");
 		driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
 		String passwordReset  = (driver.findElement(By.cssSelector("form p")).getText());
-        String newPassword = passwordReset.substring(31, 49);
-        driver.findElement(By.cssSelector("button.go-to-login-btn")).click();
-        Thread.sleep(2000);
-        
-		
-		// Login using the new password
-        
-		driver.findElement(By.xpath("//form/input[@placeHolder='Username']")).sendKeys("jessicaluz");
-		driver.findElement(By.xpath("//form/input[@placeHolder='Password']")).sendKeys(newPassword);
-		driver.findElement(By.xpath("//div/form/div/span/input[@id='chkboxOne']")).click();
-		driver.findElement(By.xpath("//div/form/div/span/input[@id='chkboxTwo']")).click();
-		driver.findElement(By.className("signInBtn")).click();
-				
+		String resetPassword = passwordReset.substring(31, 49);
+		return resetPassword;
 	}
 
 }
