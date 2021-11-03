0bc36923728b903ddd026cdb74607c0f27c5b8be
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-03 16:49:12 -0300

    Playing with AutoSuggestion Handling dropdowns

---
+++ SeleniumCourse/src/main/java/AutoSuggestiveDropdown.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutoSuggestiveDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.getProperty("webdriver.chrome.driver", "C:\\Users\\Alex\\SeleniumCourse\\SeleniumCourse\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("autosuggest")).sendKeys("ind");
+		Thread.sleep(2000);
+		List <WebElement> options = driver.findElements(By.xpath("//body/ul/li[@class='ui-menu-item']"));
+		
+		for (WebElement option : options) {
+			if(option.getText().equalsIgnoreCase("India")){
+				option.click();
+			}
+		}
+
+	}
+
+}
