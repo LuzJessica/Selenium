ff1107b7a3f0138bc1ea31636f2714f0f065e92e
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-03 17:22:18 -0300

    Learning to handle checkboxes

---
+++ SeleniumCourse/src/main/java/Checkbox.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Checkbox {
+
+	public static void main(String[] args) {
+	
+		System.getProperty("webdriver.chrome.driver", "C:\\Users\\Alex\\SeleniumCourse\\SeleniumCourse\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+		driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
+		System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+		
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+
+	}
+
+}
