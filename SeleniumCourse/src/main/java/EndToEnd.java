6b90878d8807a4ea10815a6428b99915785c9ede
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-08 20:06:22 -0300

    An End-to-end exercise to practice

---
+++ SeleniumCourse/src/main/java/EndToEnd.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class EndToEnd {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Alex\\SeleniumCourse\\SeleniumCourse\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.navigate().to("https://rahulshettyacademy.com/dropdownsPractise/#");
+		
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_0")).click();
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//li/a[@value='AMD']")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//li/a[@value='BLR']")).click();
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight")).click();
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(500);
+		for (int i=0; i<2; i++) {
+			driver.findElement(By.id("hrefIncAdt")).click();
+		}
+		driver.findElement(By.id("hrefIncChd")).click();
+		driver.findElement(By.id("hrefIncInf")).click();
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		WebElement currencyDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+		Select dropdown = new Select(currencyDropdown);
+		dropdown.selectByIndex(2);
+		driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
+		driver.findElement(By.id("ctl00_mainContent_btn_FindFlights")).click();
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
