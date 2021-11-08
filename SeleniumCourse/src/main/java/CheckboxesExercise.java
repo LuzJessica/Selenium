ce921aa7b230ba3f21193e1f6aa0c88a1c43480e
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-08 16:17:43 -0300

    Checkbox exercise

---
+++ SeleniumCourse/src/main/java/CheckboxesExercise.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class CheckboxesExercise {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Alex\\SeleniumCourse\\SeleniumCourse\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		//Assertions to check select and unselect checkbox
+		
+		Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+	
+		//Counting checkboxes
+		
+		System.out.println(driver.findElement(By.cssSelector("input[type='checkbox']")).getSize());
+	}
+
+}
