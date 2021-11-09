e408973f927f3a37faf6ed71a74b62a7b3995a4d
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-09 14:37:47 -0300

    Handling with popup alerts

---
+++ SeleniumCourse/src/main/java/Assignment.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Alex\\SeleniumCourse\\SeleniumCourse\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https:www.google.com");
+		driver.manage().window().maximize();
+		driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("name")).sendKeys("Jéssica Luz");
+		driver.findElement(By.id("alertbtn")).click();
+		Thread.sleep(1000);
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		driver.findElement(By.id("name")).sendKeys("Jéssica Luz");
+		driver.findElement(By.id("confirmbtn")).click();
+		Thread.sleep(1000);
+		driver.switchTo().alert().dismiss();
+	}
+
+}
