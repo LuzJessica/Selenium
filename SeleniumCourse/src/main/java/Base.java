32cf97a89b00b41c6ab8d608d2ee01907e6dc8d9
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-11 10:50:50 -0300

    A little bit more study about market's cart case...

--- SeleniumCourse/src/main/java/Base.java
+++ SeleniumCourse/src/main/java/Base.java
@@ -43,13 +43,21 @@ public class Base {
 			so, to guarantee that behaviour, we trim the string, is will remove all the spaces, before and after them */
 			String formattedName= name[0].trim();
 			
-			//parsing the arrar of string to a list of strings
-			List itemsNeededList = Arrays.asList(itemsNeeded);
+			/*parsing the array of string to a array list of strings. Doing this because with array list in easier to serch elements
+			inside. We can use method contain. For arrays whe do not have this. But we declare array first because array take less 
+			memories */
 			
+			List itemsNeededList = Arrays.asList(itemsNeeded);
+			//Thread.sleep(2500); in case of look to the elements of the list by text (name)
 			//if the itens of list contains one of the formatted string, so we click on ADD TO CART button
 			if(itemsNeededList.contains(formattedName)) {
+				
 				j++;
-				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+				/*do the click in a element find by te name is not the best way, because when in action this name can change.
+				If we use this case, we need to put a thread sleep, so it has time to execute the action of click and come back to
+				inicial state again. When we dont do that, the program can make a little confusion with the ids of the element in the 
+				list. Another way to solve this is not add the element by name, but by a static property, as div-button*/
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
 				/* if the value of j is equal the lenght of the array of desired items, them we stop the the process because it means
 				that all the needed items are in the cart */
 				if(j==itemsNeeded.length) {
