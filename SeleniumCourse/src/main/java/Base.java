ca16b4ec5efc1d260e406d01bc61b14485becf26
Jessica Luz <jessicaluz@gec.inatel.br>
2021-11-12 16:51:37 -0300

    Working with implicitly wait instead of Thread.sleep()

--- SeleniumCourse/src/main/java/Base.java
+++ SeleniumCourse/src/main/java/Base.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,57 +16,68 @@ public class Base {
 		String driverPath = "C:\\Users\\user\\Desktop\\Selenium\\SeleniumCourse\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		WebDriver driver = new ChromeDriver();
-		
-			
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+
 		//Create a array of strings that contains the products that I need/want to add to my cart
 		String[] itemsNeeded = {"Brocolli","Cucumber","Carrot"};
 		//controll variable to know when the number os desired items were added to cart
-		int j = 0;
+		String promoCode = "rahulshettyacademy";
+
 
 		driver.get("http://www.google.com");
 		driver.manage().window().maximize();
 		driver.navigate().to("https://rahulshettyacademy.com/seleniumPractise/#/");
 		Thread.sleep(1000);
-
+		addItems(driver,itemsNeeded);
 		
+		driver.findElement(By.cssSelector("a.cart-icon")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+		//If we do not put a wait proccess, it will fail because it takes 2 seconds +- to load.
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys(promoCode);
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
 
+	}
+	public static void addItems(WebDriver driver, String[] itemsNeeded)
+	{
+		int j = 0;
 		//receive de list of all the web elements called product-name in the website
 		List<WebElement> products =  driver.findElements(By.cssSelector("h4.product-name"));
 
 		for(int i=0 ; i<products.size();i++) {
 
 			/* variable name created to receive the name os products that were found in List of webelements. Also split that string 
-			based on de - character */
+				based on de - character */
 			String[] name = products.get(i).getText().split("-");
 			/*the name of products are like this: Brocolli - 1 Kg. When we do a .split(-), the string is divided in 2
-			 Brocolli  and 1 Kg, but the first parte of the string has a space in the end that does not appear in the string 
-			in the array. For comparation this is important, because Brocolli with space is diferent of Brocolli whitout space
-			so, to guarantee that behaviour, we trim the string, is will remove all the spaces, before and after them */
+				 Brocolli  and 1 Kg, but the first parte of the string has a space in the end that does not appear in the string 
+				in the array. For comparation this is important, because Brocolli with space is diferent of Brocolli whitout space
+				so, to guarantee that behaviour, we trim the string, is will remove all the spaces, before and after them */
 			String formattedName= name[0].trim();
-			
+
 			/*parsing the array of string to a array list of strings. Doing this because with array list in easier to serch elements
-			inside. We can use method contain. For arrays whe do not have this. But we declare array first because array take less 
-			memories */
-			
+				inside. We can use method contain. For arrays whe do not have this. But we declare array first because array take less 
+				memories */
+
 			List itemsNeededList = Arrays.asList(itemsNeeded);
 			//Thread.sleep(2500); in case of look to the elements of the list by text (name)
 			//if the itens of list contains one of the formatted string, so we click on ADD TO CART button
 			if(itemsNeededList.contains(formattedName)) {
-				
+
 				j++;
 				/*do the click in a element find by te name is not the best way, because when in action this name can change.
-				If we use this case, we need to put a thread sleep, so it has time to execute the action of click and come back to
-				inicial state again. When we dont do that, the program can make a little confusion with the ids of the element in the 
-				list. Another way to solve this is not add the element by name, but by a static property, as div-button*/
+					If we use this case, we need to put a thread sleep, so it has time to execute the action of click and come back to
+					inicial state again. When we dont do that, the program can make a little confusion with the ids of the element in the 
+					list. Another way to solve this is not add the element by name, but by a static property, as div-button*/
 				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
 				/* if the value of j is equal the lenght of the array of desired items, them we stop the the process because it means
-				that all the needed items are in the cart */
+					that all the needed items are in the cart */
 				if(j==itemsNeeded.length) {
 					break;
 				}
 			}
 		}
-
-	}
+	}		
 
 }
