3b9a0e1e95cc5335c8a1124d4e832335a66aebe1
Jessica Luz <jessuca.luz@matera.com>
2022-01-03 17:23:59 -0300

    Including and excluding methods from execution throw inclue and exclude tags

---
+++ TestNGTutorial/src/test/Day3.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day3 {
+	
+	@Test
+	public void WebLogin() {
+		//selenium
+		System.out.println("WEB");
+	}
+	
+	@Test
+	public void MobileLogin() {
+		//appium
+		System.out.println("MOBILE");
+	}
+	
+	@Test
+	public void APILogin() {
+		//rest api
+		System.out.println("API");
+	}
+
+}
---
+++ TestNGTutorial/src/test/Day4.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day4 {
+	
+	@Test
+	public void WebLoginCarLoan() {
+		//selenium
+		System.out.println("WEBCAR");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		//appium
+		System.out.println("MOBILECAR");
+	}
+	
+	@Test
+	public void APILoginCarLoan() {
+		//rest api
+		System.out.println("APICAR");
+	}
+
+}
